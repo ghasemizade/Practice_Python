dc4992c05466284b4a6ce34d9ac05750fcfe5c3c
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-02-15 16:06:21 +0330

    X pow Y 2022

---
+++ X pow Y 2022/X pow Y.py
@@ -0,0 +1,7 @@
+#this program using power function 
+#Enabling one number to another
+
+base = float(input("Enter the base of power :"))
+power = float(input("Enter the power of basa :"))
+
+print(pow(base,power))
