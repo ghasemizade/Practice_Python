46795b91b86b13bfe814ff7e706e5f9a0f02442d
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-02-15 16:05:37 +0330

    Pow 1,2,3  2022

---
+++ Pow 1,2,3  2022/Pow 1,2,3.py
@@ -0,0 +1,12 @@
+# This App takes one number from user then Reaches powers 1, 2 and 3
+
+#get input
+number = int(input("Enter number :"))
+#calculate
+pow1 = pow(number,1)
+pow2 = pow(number,2)
+pow3 = pow(number,3)
+#show numbers
+print("pow1 = %i" %pow1)
+print("pow2 = %i" %pow2)
+print("pow3 = %i" %pow3)
