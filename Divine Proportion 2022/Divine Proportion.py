88e7f21d5337d0df20e7e6ca86fb82343927f91e
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-02-19 18:12:38 +0330

    Divine Proportion 2022

---
+++ Divine Proportion 2022/Divine Proportion.py
@@ -0,0 +1,10 @@
+#this project about calculate Divine Proportion
+
+from itertools import product
+
+
+Number = int(input("Enter the number :"))
+division = Number / 1.618
+product = Number * 1.618
+print("the divine ratio of your input number : %.2f" %division)
+print("the divine ratio of your input number : %.2f" %product)
