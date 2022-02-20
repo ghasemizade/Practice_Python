bfcbac3ffda8e7dcc9d8e6304e870a9e46a566a5
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-02-20 08:41:33 +0330

    Bast pi 2022

---
+++ Bast pi 2022/Bast pi.py
@@ -0,0 +1,12 @@
+#this project about calculate bast pi
+
+
+inputnumber = int (input ("Enter the number :"))
+pi = float (3.14)
+
+
+en = 2 * pi * inputnumber
+ex = en / inputnumber
+
+
+print ("Expansion of pi number is = " , ex)
