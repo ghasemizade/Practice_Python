6473b0600718c773f5119baafeb8032ed614b6c1
Hosein Ghasemizade <hosein@ghasemizade.com>
2022-02-23 18:51:09 +0330

    empty square 2022

---
+++ empty square 2022/empty square.py
@@ -0,0 +1,20 @@
+#this project about draw hollow square
+
+#get input
+square = int (input ("Enter a size of square side :"))
+
+
+import turtle
+squ = turtle.Turtle ()
+
+squ.forward (square)
+squ.left (90)
+
+squ.forward (square)
+squ.left (90)
+
+squ.forward (square)
+squ.left (90)
+
+squ.forward (square)
+squ.left (90)
